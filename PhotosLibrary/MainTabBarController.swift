67e23949fc9633504e83ce819a9190d76a074466
Ruslan <ruslanfilistovich@gmail.com>
2019-08-19 10:37:25 +0300

    start setting

---
+++ PhotosLibrary/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  PhotosLibrary
+//
+//  Created by Ruslan on 8/19/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow 
+    }
+}
---
+++ PhotosLibrary/SceneDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SceneDelegate.swift
+//  PhotosLibrary
+//
+//  Created by Ruslan on 8/19/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+
